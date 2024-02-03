ef0e375213a4a9dbd5c47f478d44204ea9c0b827
ididdidi <ididdidi.dev@gmail.com>
2024-02-03 16:16:11 +0300

    ADD: Download test files

---
+++ Samples~/Editor/BundleCreate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+public class BundleCreate
+{
+    [MenuItem("AssetBundle/Build", false, 1)]
+    static void BuildBundles() {
+        string path = EditorUtility.SaveFolderPanel("Save Bundle", "", "");
+        if (path.Length != 0) {
+            BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        }
+    }
+}
